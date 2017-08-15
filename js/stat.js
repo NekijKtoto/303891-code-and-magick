04b1248876675c36323c40e16d317aafb4750ffd
Daria Glagol <daria.glagol@gmail.com>
2017-08-15 23:51:14 +0700

    Тревис доволен

--- js/stat.js
+++ js/stat.js
@@ -2,7 +2,7 @@
 
 window.renderStatistics = function (ctx, names, times) {
   ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
-  ctx.fillRect(110, 20, 420., 270);
+  ctx.fillRect(110, 20, 420, 270);
   ctx.fillStyle = 'rgba(255, 255, 255, 1.0)';
   ctx.fillRect(100, 10, 420, 270);
   ctx.fillStyle = '#000';
