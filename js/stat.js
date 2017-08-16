13a76e169e42636cf1158b740896991fa341f095
Daria Glagol <daria.glagol@gmail.com>
2017-08-16 21:41:46 +0700

    Оптимизация кода

--- js/stat.js
+++ js/stat.js
@@ -1,18 +1,34 @@
 'use strict';
 
 window.renderStatistics = function (ctx, names, times) {
-  function drawRect (x,y, width, height) {
-    //ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
-    //ctx.fillRect(110, 20, 420, 270);
-    ctx.fillStyle = 'rgba(255, 255, 255, 1.0)';
-    ctx.fillRect = (x, y, width, height);
-     return ctx.fillRect;
+  
+  var histogramHeight = 150;
+  var barHeigth = 40;
+  var indent = 90;
+  var initialX = 125;
+  var initialY = 250;
+  
+  var randomBlue = function () {
+    return 'rgba(0, 0, 255, ' + Math.random().toString() + ')';
   };
-  var rect = drawRect(100, 10, 420, 270);
-  ctx.fillStyle = '#000';
-  ctx.font = '16px PT Mono';
-  ctx.fillText('Ура, вы победили! ', 120, 45);
-  ctx.fillText('Список результатов: ', 120, 65);
+  
+  var drawRect = function (x, y, width, height, color) {
+    ctx.fillStyle = color || '#ffffff';
+    ctx.fillRect(x, y, width, height);
+  };
+  
+  var drawText = function (text, x, y, color, font) {
+    ctx.fillStyle = color || '#000000';
+    ctx.textBaseline = 'hanging';
+    ctx.font = font || '16 px PT Mono';
+    ctx.fillText(text, x, y);
+  };
+  
+  drawRect(110, 20, 420, 270, 'rgba(0, 0, 0, 0.7)');
+  drawRect(100, 10, 420, 270);
+  drawText('Ура, вы победили!', 120, 45);
+  drawText('Список результатов: ', 120, 65);
+  
   var max = -1;
   for (var i = 0; i < times.length; i++) {
     var time = times[i];
@@ -20,23 +36,13 @@ window.renderStatistics = function (ctx, names, times) {
       max = time;
     }
   }
-  var histogramHeight = 150;
-  var step = histogramHeight / max;
-  var barHeigth = 40;
-  var indent = 90;
-  var initialX = 125;
-  var initialY = 250;
-  ctx.textBaseline = 'hanging';
-  for (var t = 0; t < times.length; t++) {
-    ctx.fillStyle = '#000';
-    ctx.fillText(names[t], initialX + indent * t, initialY + 10);
-    ctx.fillText(Math.floor(times[t]), initialX + indent * t, initialY - (times[t] * step) - 20);
-    if (names[t] === 'Вы') {
-      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-      ctx.fillRect(initialX + indent * t, initialY, barHeigth, times[t] * step * (-1));
-    } else {
-      ctx.fillStyle = 'rgba(0, 0, 255, ' + (Math.random() + 0.4) / 2 + ')';
-      ctx.fillRect(initialX + indent * t, initialY, barHeigth, times[t] * step * (-1));
-    }
+    var step = histogramHeight / max;
+
+    for (var t = 0; t < times.length; t++) {
+
+    drawText(names[t], initialX + indent * t, initialY + 10);
+    drawText(Math.floor(times[t]), initialX + indent * t, initialY - (times[t] * step) - 20);
+
+    drawRect(initialX + indent * t, initialY, barHeigth, times[t] * step * (-1), (names[t] === 'Вы') ? '#FF0000' : randomBlue());
   }
 };
