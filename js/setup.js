fd259e663c1d1b059ffc6d4343f70621a57da518
Daria Glagol <daria.glagol@gmail.com>
2017-08-20 22:37:58 +0700

    Тревис уть-чуть доволен

--- js/setup.js
+++ js/setup.js
@@ -8,45 +8,23 @@ var setupSimilarLabel = similarWizardTemplate.querySelector('.setup-similar-labe
 var wizardCoat = similarWizardTemplate.querySelector('.wizard-coat');
 var wizardEye = similarWizardTemplate.querySelector('.wizard-eyes');
 var wizards = [
-  {
-    name:  ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон' ],
-    surname:  ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'],
-    coatColor: ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'],
-    eyesColor: ['black', 'red', 'blue', 'yellow', 'green']
-},
-  {
-    name:  ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон' ],
-    surname:  ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'],
-    coatColor: ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'],
-    eyesColor: ['black', 'red', 'blue', 'yellow', 'green']
-},
-  {
-    name:  ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон' ],
-    surname:  ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'],
-    coatColor: ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'],
-    eyesColor: ['black', 'red', 'blue', 'yellow', 'green']
-},
-  {
-    name:  ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон' ],
-    surname:  ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'],
-    coatColor: ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'],
-    eyesColor: ['black', 'red', 'blue', 'yellow', 'green']
-}
+  {name: ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'], surname: ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'], coatColor: ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'], eyesColor: ['black', 'red', 'blue', 'yellow', 'green']},
+  {name:  ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'], surname: ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'], coatColor: ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'], eyesColor: ['black', 'red', 'blue', 'yellow', 'green']},
+  {name: ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'], surname: ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'], coatColor: ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'], eyesColor: ['black', 'red', 'blue', 'yellow', 'green']},
+  {name: ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'], surname: ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'], coatColor: ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'], eyesColor: ['black', 'red', 'blue', 'yellow', 'green']}
 ];
-
 var renderWizard = function (randName, randSurname, randCoat, randEyes) {
   similarWizardTemplate.cloneNode(true);
   for (var i = 0; i < 4; i++) {
-    var randName = Math.floor(Math.random() * wizards[i].name.length);
-    var randSurname = Math.floor(Math.random() * wizards[i].surname.length);
-    var randCoat = Math.floor(Math.random() * wizards[i].coatColor.length);
-    var randEyes = Math.floor(Math.random() * wizards[i].eyesColor.length);
+    randName = Math.floor(Math.random() * wizards[i].name.length);
+    randSurname = Math.floor(Math.random() * wizards[i].surname.length);
+    randCoat = Math.floor(Math.random() * wizards[i].coatColor.length);
+    randEyes = Math.floor(Math.random() * wizards[i].eyesColor.length);
     setupSimilarLabel.textContent = wizards[i].name[randName] + ' ' + wizards[i].surname[randSurname];
     wizardCoat.setAttribute('style', 'fill: '+ wizards[i].coatColor[randCoat] + ';');
     wizardEye.setAttribute('style', 'fill: ' + wizards[i].eyesColor[randEyes] + ';');
   }
 };
-
 var fragment = document.createDocumentFragment();
 for (var i = 0; i < 4; i++) {
   fragment.appendChild(renderWizard(randName, randSurname, randCoat, randEyes));
